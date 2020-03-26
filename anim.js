a2fa90f81e168c78cb11e32e5d93e3a820664157
damienfs <damiencherfils@gmail.com>
2020-03-26 18:17:06 +0100

    ajout d'un alert

--- anim.js
+++ anim.js
@@ -1,5 +1,5 @@
 $('document').ready(function(){
-
+    alert('En poussant les images par la gauche vous pourrez les déplacer');
     $("img").mouseover(function(){
 
     $(this).css('transform','rotate(10deg)');
